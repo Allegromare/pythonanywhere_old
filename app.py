a6383a1677730eb0e77319d5ecc88f553ee63bed
Allegromare <allegromare@gmail.com>
2022-06-09 09:47:00 +0200

    tirth

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+import git  # GitPython library
 
 app = Flask(__name__)
+
+
+# Route for the GitHub webhook
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./orbe')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 @app.route('/')
 def index():
